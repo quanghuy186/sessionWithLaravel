b9d992bfca8fe5f9c2aa2fa185650ee6453aa4f1
LAPTOP-RSH17G7M\LENOVO <quanghuy180623@gmail.com>
2024-02-27 21:55:35 +0700

    add cookie

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,17 +6,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    function showFormLogin(){
-        return view('login');
+    function showFormLogin(Request $request){
+        $email = $request->cookie('email');
+        return view('login', compact('email'));
     }
 
     function login(Request $request)  {
         $email = $request->email;
         $password = $request->password;
-        if($email == 'admin@gmail.com' && $password == '1234'){
+        if($email == 'huy@gmail.com' && $password == '1234'){
             // tạo session lưu email đăng nhập
-            session('userEmail', $email);
-            return redirect()->route('home');
+            session()->put('userEmail', $email);
+            // session('userEmail', $email);
+            $cookie = cookie('email', $email, 60);
+            return redirect()->route('home')->cookie($cookie);
         } else {
             // session flash chỉ được tồn tại duy nhất trong một request
             session()->flash('error', 'Account not exist');
